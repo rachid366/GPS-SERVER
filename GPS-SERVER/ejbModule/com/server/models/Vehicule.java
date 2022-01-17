71f93f59ebc79d764708bcde8b23475f69ca6430
rachid366 <mohamedrachid286@gmail.com>
2022-01-17 10:00:19 +0100

    ajouter server project

---
+++ GPS-SERVER/ejbModule/com/server/models/Vehicule.java
@@ -0,0 +1,107 @@
+package com.server.models;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vehicule implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 295379757362210520L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String matricule;
+	private String marque;
+	private String type;
+	private int vitesse;
+	
+	//@OneToMany(mappedBy = "vehicule")
+	//private Set<VehiculeGPSTracker> VehiculeGPSTracker;
+
+	public Vehicule() {
+		super();
+	}
+
+	
+
+
+	public Vehicule(int id, String matricule, String marque, String type, int vitesse) {
+		super();
+		this.id = id;
+		this.matricule = matricule;
+		this.marque = marque;
+		this.type = type;
+		this.vitesse = vitesse;
+	}
+
+
+
+
+	public Vehicule(String matricule, String marque, String type, int vitesse) {
+		super();
+		this.matricule = matricule;
+		this.marque = marque;
+		this.type = type;
+		this.vitesse = vitesse;
+	}
+
+
+
+
+	public int getVitesse() {
+		return vitesse;
+	}
+
+
+
+
+	public void setVitesse(int vitesse) {
+		this.vitesse = vitesse;
+	}
+
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMatricule() {
+		return matricule;
+	}
+
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+
+	public String getMarque() {
+		return marque;
+	}
+
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
